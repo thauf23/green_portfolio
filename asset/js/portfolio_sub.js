15319e50c2708b9316ec5da7bd87dc63b52b5da1
thauf23 <thauf23@naver.com>
2020-05-28 12:10:42 +0900

    20.05.28.12.10

---
+++ asset/js/portfolio_sub.js
@@ -0,0 +1,15 @@
+$(function(){
+    // start
+    // var h2 = $('h2');
+    // var span = $('h2 span');
+    // console.log(span);
+    h2move()
+    function h2move(){
+        console.log("aa");
+        $('h2 span').animate({
+            top: 0
+        },1000);
+    }
+
+    // end
+});
--- asset/php/portfolio_Shinil.php
+++ asset/php/portfolio_Shinil.php
@@ -8,6 +8,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>    
     <script src="/asset/js/portfolio_buger.js"></script>
     <script src="/asset/js/header_absolute.js"></script>
+    <script src="/asset/js/portfolio_sub.js"></script>
 </head>
 <body>
     <div id="warp">
@@ -20,8 +21,8 @@
     <div class="container">
         <section class="shinil_visual parents">
             <h2>
-                <span>Shinil</span>
-                <span>polytech</span>
+                <span>Shinil p</span>
+                <span>olytech</span>
             </h2>
             <p><img src="../img/shinil/shinil_visual.jpg" alt="shinil img"></p>
             <div class="scroll_down">
