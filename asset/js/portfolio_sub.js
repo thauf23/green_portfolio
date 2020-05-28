2993a069364c3817a667065b6cc33334d5e252f5
thauf23 <thauf23@naver.com>
2020-05-28 13:20:33 +0900

    20.05.28.13.20

--- asset/js/portfolio_sub.js
+++ asset/js/portfolio_sub.js
@@ -1,26 +1,7 @@
 $(function(){
     // start
 
-    // h2move()
-    function h2move(){
-        $('h2 span').animate({
-            opacity: 1,
-            top: 0
-        },1300,function(){
-            $('h2 span').animate({
-                opacity: ".2"
-            },100,function(){
-                $('h2 span').animate({
-                    opacity: 1,
-                    "text-shadow": "5px 8px 10px rgba(203,203,203,0.7)"
-                },500,function(){
-                    $('.scroll_down').animate({
-                        opacity: 1
-                    });
-                });
-            });
-        });
-    }
 
+    
     // end
 });
