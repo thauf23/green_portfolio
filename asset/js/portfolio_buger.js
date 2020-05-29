a0d5689b2ac68f908a4a406a52c291946bee0b10
thauf23 <thauf23@naver.com>
2020-05-29 13:17:11 +0900

    20.05.29.13.17

--- asset/js/portfolio_buger.js
+++ asset/js/portfolio_buger.js
@@ -1,23 +1,29 @@
 $(function(){
     // start
     $('.header_menu').on('click', function(){
+        var pinK = "_pink";
+        var whitE = "_white";
+        var logoI = $('header h1 a img');
+        var imgS, chAn;
+
         $(".box").slideToggle(600);
         $('header h1 a').hide().toggleClass('color');
+        imgS = logoI.attr('src');
         $('header h1 a').fadeIn(1000);
         
+
         if($('header h1 a').attr("class")){
             $('header nav').hide();
-            // setTimeout(function(){
-                // $('header').css("position", "fixed");
-                // $('.header_menu').css("background","#1c1b34");
-            // },500);
+            chAn = imgS.replace(pinK,whitE);
+            logoI.attr('src',chAn);
             $('.header_menu').hide().css("background","#fff");
             $('.header_menu span').css("background","#ff484a");
             $('.header_menu').fadeIn()
         }else{
-            // $('header').css("position", "relative");
             setTimeout(function(){
                 $('header nav').fadeIn();
+                chAn = imgS.replace(whitE,pinK);
+                logoI.attr('src',chAn);
                 $('.header_menu').css("background","#ff484a");
                 $('.header_menu span').css("background","#fff");
             },400);
